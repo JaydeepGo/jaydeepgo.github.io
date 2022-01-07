cd8e3836fd85f2eee940b83c8eee150c6c68ad5c
JaydeepGo <jaydeep415@gmail.com>
2022-01-08 03:36:32 +0530

    feat -  implement tag navigation page

---
+++ src/_11ty/getTagList.js
@@ -0,0 +1,23 @@
+const excludedTags = ["all", "nav", "post", "posts"]
+const externalPosts = require('../_data/external-posts.js')
+
+module.exports = function(collection) {
+  let tagSet = new Set();
+  collection.getAll().forEach(function(item) {
+    if( "tags" in item.data ) {
+      let tags = item.data.tags.filter(item => !excludedTags.includes(item));
+
+      for (const tag of tags) {
+        tagSet.add(tag);
+      }
+    }
+  });
+  externalPosts.forEach(item => {
+    for (const tag of item.tags) {
+      tagSet.add(tag)
+    }
+  })
+
+  // returning an array in addCollection works in Eleventy 0.5.3
+  return [...tagSet];
+};
