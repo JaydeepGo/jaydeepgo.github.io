6777638ac698142c94c59fba06441b610d2ba07e
JaydeepGo <jaydeep415@gmail.com>
2022-07-17 06:06:29 +0530

    feat - preview link implementation done

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,6 +3,8 @@ module.exports = {
   author: 'JaydeepGo',
   url: 'https://jaydeepgo.com',
   email: 'jaydeep@mail.com',
+  summary: 'I do random stuff.',
+  profilePic: 'pc.jpg',
   rss: {
     title: 'heeee',
     subtitle: 'I am writing about my experiences as a naval navel-gazer.',
